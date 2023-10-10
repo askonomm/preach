5106809f7db117abbe37bfb80f9710b79da368b3
Asko Nõmm <asko@repl.ee>
2023-10-11 02:58:39 +0300

    bump

--- static/js/site.js
+++ static/js/site.js
@@ -1,7 +1,25 @@
 window.addEventListener("scroll", (e) => {
-    if (window.scrollY > 0) {
+    if (window.innerWidth > 800) {
+        if (window.scrollY > 0) {
+            document.querySelector(".header").classList.add("is-visible");
+        } else {
+            document.querySelector(".header").classList.remove("is-visible");
+        }
+    }
+})
+
+window.addEventListener("load", () => {
+    if (window.innerWidth < 800) {
         document.querySelector(".header").classList.add("is-visible");
-    } else {
+    }
+})
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth < 800) {
+        document.querySelector(".header").classList.add("is-visible");
+    }
+
+    if (window.innerWidth > 800 && window.scrollY === 0) {
         document.querySelector(".header").classList.remove("is-visible");
     }
 })
